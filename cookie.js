7a3f6e54115b6e930fa66575e7f25d66812d3f6e
Marian Wolf <marian.wolf2008@gmail.com>
2025-09-22 22:07:41 +0200

    passe Farb- und Abstandsvariablen an, verbessere die Schriftgrößen und füge neue Hintergrundfarben hinzu

--- cookie.js
+++ cookie.js
@@ -1,16 +1,3 @@
-const SCRIPT_CONFIGURATIONS = {
-    vercel: [
-        { textContent: 'window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };' },
-        { defer: true, src: '/_vercel/insights/script.js' },
-        { textContent: 'window.si = window.si || function () { (window.siq = window.siq || []).push(arguments); };' },
-        { defer: true, src: '/_vercel/speed-insights/script.js' },
-    ],
-    googleAnalytics: [
-        { textContent: "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-93C0KPGRPJ');" },
-        { defer: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-93C0KPGRPJ' },
-    ],
-};
-
 const cookieBanner = document.getElementById('cookieBanner');
 const acceptButton = document.getElementById('acceptCookies');
 const declineButton = document.getElementById('declineCookies');
@@ -28,22 +15,19 @@ if (cookieBanner && acceptButton && declineButton) {
         return null;
     };
 
-    const createScript = (scriptProps) => {
-        const script = document.createElement('script');
-        Object.assign(script, scriptProps);
-        document.head.appendChild(script);
-    };
-
-    const loadScripts = (key) => {
-        const scriptsToLoad = SCRIPT_CONFIGURATIONS[key];
-        if (scriptsToLoad && !document.head.querySelector(`script[src="${scriptsToLoad[1].src}"]`)) {
-            scriptsToLoad.forEach(createScript);
-        }
-    };
-
     const enableAllScripts = () => {
-        loadScripts('vercel');
-        loadScripts('googleAnalytics');
+        window.va = window.va || function () {
+            (window.vaq = window.vaq || []).push(arguments);
+        };
+        window.si = window.si || function () {
+            (window.siq = window.siq || []).push(arguments);
+        };
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'G-93C0KPGRPJ');
     };
 
     const handleAcceptAction = () => {
