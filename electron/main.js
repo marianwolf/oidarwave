f5d8a6cd08694ee48d314db835bd4a2c244e3ddd
marianwolf <marian.wolf2008@gmail.com>
2026-05-05 17:07:47 +0200

    build(electron): update build configuration and ignore patterns

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow } = require('electron');
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 
 function createWindow() {
